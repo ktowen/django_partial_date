54bf645698cecebf238fe8c4ea6294c050b559b7
ktowen <pedroall21@gmail.com>
2018-04-17 11:39:53 -0400

    Added django 2 support

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='django_partial_date',
-    version='1.2.0',
+    version='1.2.1',
 
     description='Django custom model field for partial dates with the form YYYY, YYYY-MM, YYYY-MM-DD',
     long_description=long_description,
@@ -33,6 +33,6 @@ setup(
     packages=['partial_date'],
 
     install_requires=[
-        'Django <2.0.0',
+        'django',
     ],
 )
