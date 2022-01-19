485b02889c1a047c8a110faba73913f0e6b36762
Sorin Marti <32014438+sorinmarti@users.noreply.github.com>
2022-01-19 10:07:37 +0100

    Replaced import ugettext_lazy with gettext_lazy
    ugettext_lazy was removed in Django 4.0: https://docs.djangoproject.com/en/dev/internals/deprecation/#deprecation-removed-in-4-0

--- partial_date/fields.py
+++ partial_date/fields.py
@@ -6,7 +6,7 @@ import six
 
 from django.core import exceptions
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 partial_date_re = re.compile(
